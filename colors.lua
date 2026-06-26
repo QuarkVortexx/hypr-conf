187a4bd130b6f0dcd0668662cb6a4e025cd7dbf6
QuarkVortexx <quarkvortex@outlook.com>
2026-06-26 04:53:14 +0300

    Update to lua

---
+++ colors.lua
@@ -0,0 +1,120 @@
+return {
+    image = "/home/laptop/Pictures/Wallpapers/wallpaper1.jpg",
+
+    background = "0xff19120d",
+
+    error = "0xffffb4a8",
+
+    error_container = "0xff73342a",
+
+    error_source = "0xffff0000",
+
+    error_value = "0xffff0000",
+
+    inverse_on_surface = "0xff382f28",
+
+    inverse_primary = "0xff89511f",
+
+    inverse_surface = "0xffefdfd6",
+
+    on_background = "0xffefdfd6",
+
+    on_error = "0xff561e16",
+
+    on_error_container = "0xffffdad4",
+
+    on_primary = "0xff4e2600",
+
+    on_primary_container = "0xffffdcc4",
+
+    on_primary_fixed = "0xff2f1500",
+
+    on_primary_fixed_variant = "0xff6c3a08",
+
+    on_secondary = "0xff422b1a",
+
+    on_secondary_container = "0xffffdcc4",
+
+    on_secondary_fixed = "0xff2a1707",
+
+    on_secondary_fixed_variant = "0xff5b412f",
+
+    on_surface = "0xffefdfd6",
+
+    on_surface_variant = "0xffd6c3b7",
+
+    on_tertiary = "0xff2f330c",
+
+    on_tertiary_container = "0xffe2e7af",
+
+    on_tertiary_fixed = "0xff1a1e00",
+
+    on_tertiary_fixed_variant = "0xff454a21",
+
+    on_warning = "0xff3b2f00",
+
+    on_warning_container = "0xfffce186",
+
+    outline = "0xff9f8d82",
+
+    outline_variant = "0xff51443b",
+
+    primary = "0xffffb780",
+
+    primary_container = "0xff6c3a08",
+
+    primary_fixed = "0xffffdcc4",
+
+    primary_fixed_dim = "0xffffb780",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffe4c0a6",
+
+    secondary_container = "0xff5b412f",
+
+    secondary_fixed = "0xffffdcc4",
+
+    secondary_fixed_dim = "0xffe4c0a6",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff9e602b",
+
+    surface = "0xff19120d",
+
+    surface_bright = "0xff413731",
+
+    surface_container = "0xff261e18",
+
+    surface_container_high = "0xff312822",
+
+    surface_container_highest = "0xff3c332d",
+
+    surface_container_low = "0xff221a14",
+
+    surface_container_lowest = "0xff140d08",
+
+    surface_dim = "0xff19120d",
+
+    surface_tint = "0xffffb780",
+
+    surface_variant = "0xff51443b",
+
+    tertiary = "0xffc6cb95",
+
+    tertiary_container = "0xff454a21",
+
+    tertiary_fixed = "0xffe2e7af",
+
+    tertiary_fixed_dim = "0xffc6cb95",
+
+    warning = "0xffdec56e",
+
+    warning_container = "0xff554500",
+
+    warning_source = "0xffffff00",
+
+    warning_value = "0xffffff00",
+
+}
